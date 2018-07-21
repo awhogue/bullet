17ae0dbd74ea22bfc804e3022fa28efcffcd267a
Andrew Hogue <ahogue@gmail.com>
2018-07-21 14:59:20 -0400

    Add fields to BulletRow to represent type information.

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,10 +1,39 @@
 // Models for the bullet journal.
 
-// Represents a single row in the journal, e.g. "Hours slept" or "Coffee consumed".
-// NOTE: not currently used – bring it back when creating new rows with comments and units.
+// Types that the data in an entry can take.
+enum BulletRowDataType {
+  // A boolean value – did I do this today or not?
+  Checkmark,
+  // A numeric value, e.g. how many cups of coffee did I drink?
+  Number,
+  // A number that falls within a certain range, e.g. "Energy" on a scale of 0-10.
+  RangedNumber,
+  // A row that's just free text.
+  FreeText,
+  // TODO: add an "Enumeration" type that limits a row to one of a small set of values.
+}
+
+// Ways a row can handle multiple entries in the same day.
+enum BulletRowMultiEntryType {
+  // Subsequent entries in the same day will overwrite the old value.
+  Overwrite,
+  // Subsequent entries are stored separately, but display as an accumulation. 
+  // E.g. Entering "Coffee: 1" twice in the same day displays as "2"
+  // E.g. Entering "Workout: X" twice in the same day displays as "XX"
+  Accumulate,
+  // Additional entries are stored and displayed as separate.
+  // E.g. Entering "Energy: 4" and "Energy: 7" in the same day will show as two separate entries.
+  Separate,
+}
+
+// Represents a single row in the journal, e.g. "Sleep" or "Coffee" or "Work out."
 class BulletRow {
   // The name of the row, e.g. "Coffee" or "Sleep"
   final String name;
+  // What type of row this is.
+  final BulletRowDataType dataType;
+  // How to treat multiple entries for this row.
+  final BulletRowMultiEntryType multiEntryType;
   // The units for the row, e.g. "mg" or "hours".
   final String units;
   // A comment for the row.
@@ -18,7 +47,8 @@ class BulletEntry {
   final String value;
   // The date the entry happened.
   final DateTime entryDate;
-  // The name of the row this entry is in. (Eventually this may need to be an actual reference to a BulletRow.)
+  // The name of the row this entry is in. For now, this is just a string that is identical to 
+  // the "name" field of BulletRow. Someday we'll make this a real ID.
   final String rowName;
   // A comment for the entry.
   final String comment;
